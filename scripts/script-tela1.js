17a03fcfb9e392a2ecde9e1960f65400d3dd1dec
Analice Dias <anadias.alice@gmail.com>
2022-02-17 20:12:21 -0300

    Tela 1 - carregar lista de quizzes: Todos

--- scripts/script-tela1.js
+++ scripts/script-tela1.js
@@ -1,9 +1,30 @@
 const URL_BUZZQUIZZ = "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes"
 
+function inserirQuizzesNaTela(lista){
+let listaDeQuizzes = lista.data;
+let secaoDeTodosOsQuizzes = document.querySelector(".container-tela1 section.todos-os-quizzes");
+
+secaoDeTodosOsQuizzes.innerHTML="<h3>Todos os Quizzes</h3>"
+
+for(let i=0; i<listaDeQuizzes.length; i++){
+    secaoDeTodosOsQuizzes.innerHTML+=`
+        <article>
+            <img src="${listaDeQuizzes[i].image}">
+            <div class="sombra"></div>   
+            <a>${listaDeQuizzes[i].title}</a>              
+        </article>
+    `
+}
+
+}
+
 function solicitarTodosOsQuizzes(){
 let requisicao = axios.get(URL_BUZZQUIZZ);
-requisicao.than();
-requisicao.catch();
+
+requisicao.then(inserirQuizzesNaTela);
+requisicao.catch(
+    ()=> console.log("Erro ao solitar lista de quizzes")
+);
 
 }
 
